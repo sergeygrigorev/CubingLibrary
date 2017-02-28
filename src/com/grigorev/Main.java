07184a25f12388f565d2035f7d8fc6dfa36a18e3
GrigorevSergey <unstope@gmail.com>
2017-02-28 23:00:27 +0300

    Initial commit

---
+++ src/com/grigorev/Main.java
@@ -0,0 +1,10 @@
+package com.grigorev;
+
+import java.util.function.BiFunction;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("adfadf");
+    }
+}
